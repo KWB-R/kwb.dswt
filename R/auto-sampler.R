250d606a5af642e58430f94f367130735f5ac997
hsonne <hauke.sonnenberg@gmx.de>
2019-02-20 18:26:20 +0100

    Clean auto-sampler.r
    - use kwb.utils::catAndRun()
    - use seq_len(n) or seq_along(x) instead of 1:n or
      1:length(x)
    - improve indentation and spacing

--- R/auto-sampler.R
+++ R/auto-sampler.R
@@ -4,26 +4,30 @@
 #' @keywords internal
 readDswtSamplerFileByName <- function(samplerFile, bottlesToConsider)
 {
-  cat(sprintf("Reading sample data from \"%s\"... ", basename(samplerFile)))
-
-  sampleData.samples <- kwb.logger::readLogger_Ori_BasicEx1(
-    samplerFile,
-    infotype = "times")
-
-  sampleData.actions <- kwb.logger::readLogger_Ori_BasicEx1(
-    samplerFile,
-    infotype = "actions")
-
-  cat("ok.\n")
+  kwb.utils::catAndRun(
+    sprintf("Reading sample data from \"%s\"", basename(samplerFile)), 
+    expr = {
+      sampleData.samples <- kwb.logger::readLogger_Ori_BasicEx1(
+        samplerFile, infotype = "times"
+      )
+      sampleData.actions <- kwb.logger::readLogger_Ori_BasicEx1(
+        samplerFile, infotype = "actions"
+      )
+    }
+  )
 
   if (kwb.utils::isNullOrEmpty(sampleData.samples)) {
-    warning("\n\n*** No samples in ", basename(samplerFile),
-            "! The file is skipped.\n")
+    
+    warning(sprintf(
+      "\n\n*** No samples in %s! The file is skipped.\n", basename(samplerFile)
+    ))
+    
     return (NULL)
   }
 
   # remove empty lines and trim action names
-  sampleData.actions <- sampleData.actions[!is.na(sampleData.actions$myDateTime), ]
+  time_available <- ! is.na(sampleData.actions$myDateTime)
+  sampleData.actions <- sampleData.actions[time_available, ]
   sampleData.actions$Ereignis <- kwb.utils::hsTrim(sampleData.actions$Ereignis)
   sampleData.actions$Flasche <- kwb.utils::hsTrim(sampleData.actions$Flasche)
 
@@ -32,7 +36,8 @@ readDswtSamplerFileByName <- function(samplerFile, bottlesToConsider)
   sampleData.actions <- removeLinesBeforeYear(sampleData.actions, 2013)
 
   sampleData.samples <- kwb.utils::removeColumns(
-    sampleData.samples, c("Akku", "V9", "V10"))
+    sampleData.samples, c("Akku", "V9", "V10")
+  )
 
   # identify sample events that are directly followed by the error "no water"
   rows <- grep("Kein Wasser", sampleData.actions$Ereignis)
@@ -48,20 +53,22 @@ readDswtSamplerFileByName <- function(samplerFile, bottlesToConsider)
       secondsBefore >= 0
       & secondsBefore < 10
       & as.integer(sampleData.samples$Flasche)
-      == as.integer(sampleData.noWater$Flasche[i]))
+      == as.integer(sampleData.noWater$Flasche[i])
+    )
 
     ifelse(length(row.sample) > 0, max(row.sample), -1)
   }
 
   sampleIndices.noWater <- sapply(
-    1:nrow(sampleData.noWater),
-    FUN = noWaterIndexToSampleIndex)
+    seq_len(nrow(sampleData.noWater)), FUN = noWaterIndexToSampleIndex
+  )
 
   sampleIndices.noWater <- setdiff(sampleIndices.noWater, -1)
 
   sampleData.samples$result <- "SUCCESS"
 
-  if (!kwb.utils::isNullOrEmpty(sampleIndices.noWater)) {
+  if (! kwb.utils::isNullOrEmpty(sampleIndices.noWater)) {
+    
     sampleData.samples$result[sampleIndices.noWater] <- "NO WATER"
   }
 
@@ -71,19 +78,22 @@ readDswtSamplerFileByName <- function(samplerFile, bottlesToConsider)
   sampleDataExtended <- cbind(
     samplerFile = basename(samplerFile),
     sampleData.samples,
-    stringsAsFactors = FALSE)
+    stringsAsFactors = FALSE
+  )
 
-  sampleDataExtended <- kwb.utils::hsRenameColumns(
-    sampleDataExtended,
-    list(myDateTime = "sampleTime",
-         Flasche = "bottle",
-         Fuellmenge = "volume"))
-
+  sampleDataExtended <- kwb.utils::renameColumns(sampleDataExtended, list(
+    myDateTime = "sampleTime",
+    Flasche = "bottle",
+    Fuellmenge = "volume"
+  ))
+  
   # filter for relevant bottles
   if (! all(is.na(bottlesToConsider))) {
+    
     indices <- which(sampleDataExtended$bottle %in% bottlesToConsider)
 
     if (kwb.utils::isNullOrEmpty(indices)) {
+      
       stop(sprintf(
         "No of the existing bottles (%s) are to be considered (%s).",
         kwb.utils::commaCollapsed(sampleDataExtended$bottle),
@@ -104,13 +114,20 @@ removeLinesBeforeYear <- function(sampleData, year)
 {
   before <- which(as.integer(substr(sampleData$myDateTime, 1, 4)) < year)
 
-  if (!kwb.utils::isNullOrEmpty(before)) {
-    message("\n*** ", length(before), " lines with dates before ", year, " were removed:\n")
+  if (! kwb.utils::isNullOrEmpty(before)) {
+    
+    message(sprintf(
+      "\n*** %d lines with dates before %d were removed:\n", 
+      length(before), year
+    ))
+    
     print(sampleData[before, ])
 
-    warning(length(before), " lines with dates before ", year, " were removed ",
-            "(see above).")
-
+    warning(sprintf(
+      "%d lines with dates before %d were removed (see above).", 
+      length(before), year
+    ))
+    
     sampleData <- sampleData[-before, ]
   }
 
@@ -146,7 +163,7 @@ readAndPlotAutoSamplerFiles <- function(
       removePattern, unique(all.actions$Ereignis), value = TRUE
     )
 
-    actions <- all.actions[!all.actions$Ereignis %in% actions.to.remove, ]
+    actions <- all.actions[! all.actions$Ereignis %in% actions.to.remove, ]
   }
 
   # Plot all auto sampler actions
@@ -164,8 +181,10 @@ getActionsFromAutoSamplerFiles <- function(pnFiles)
 {
   all.actions <- NULL
 
-  for (i in 1:length(pnFiles)) {
+  for (i in seq_along(pnFiles)) {
+    
     actions <- getActionsFromAutoSamplerFile(pnFiles[i], i)
+    
     all.actions <- rbind(all.actions, actions)
   }
 
@@ -188,23 +207,23 @@ getActionsFromAutoSamplerFile <- function(
 {
   stopifnot(length(pnFile) == 1)
 
-  cat("Reading file", fileNumber, ":", pnFile, "...")
-  actions <- kwb.logger::readLogger_Ori_BasicEx1(pnFile, infotype="actions")
-  cat("ok.\n")
-
+  kwb.utils::catAndRun(sprintf("Reading file %d: %s", fileNumber, pnFile), {
+    actions <- kwb.logger::readLogger_Ori_BasicEx1(pnFile, infotype = "actions")
+  })
+  
   # Remove rows without time
-  actions <- actions[!is.na(actions$myDateTime), ]
+  actions <- actions[! is.na(actions$myDateTime), ]
 
   if (kwb.utils::isNullOrEmpty(actions)) {
-    warning("No actions in ", pnFile, ".")
+    warning(sprintf("No actions in %s.", pnFile))
     return(NULL)
   }
 
   # Remove errors (if required)
   if (remove.errors) {
-    actions <- actions[grep("Fehler", actions$Ereignis, invert=TRUE), ]
+    actions <- actions[! grepl("Fehler", actions$Ereignis), ]
     if (kwb.utils::isNullOrEmpty(actions)) {
-      warning("No non-error actions in ", pnFile, ".")
+      warning(sprintf("No non-error actions in %s.", pnFile))
       return(NULL)
     }
   }
@@ -215,34 +234,45 @@ getActionsFromAutoSamplerFile <- function(
   # Look for times before 2013
   invalid <- which(as.POSIXlt(actions$myDateTime)$year + 1900 < 2012)
 
-  if (!kwb.utils::isNullOrEmpty(invalid)) {
-    warning("There are invalid timestamps (before 2012) in ", pnFile, "\n",
-            "  that were removed:\n  ",
-            paste(actions$myDateTime[invalid], collapse="\n  "))
-
+  if (! kwb.utils::isNullOrEmpty(invalid)) {
+    
+    warning(
+      sprintf("There are invalid timestamps (before 2012) in %s\n", pnFile),
+      "  that were removed:\n  ",
+      paste(actions$myDateTime[invalid], collapse = "\n  ")
+    )
+    
     actions <- actions[-invalid, ]
   }
 
   # order by timestamps and give a warning if actions were not ordered
   timeOrder <- order(actions$myDateTime)
-  if (any(timeOrder != 1:nrow(actions))) {
-    warning("Not all actions were ordered by time in ", pnFile, "\n",
-            "  and have been ordered.")
+  
+  if (any(timeOrder != seq_len(nrow(actions)))) {
+    
+    warning(
+      sprintf("Not all actions were ordered by time in %s\n", pnFile),
+      "  and have been ordered."
+    )
+    
     actions <- actions[timeOrder, ]
   }
 
   actions$Ereignis <- kwb.utils::hsTrim(actions$Ereignis)
 
   # Define required columns
-  columns <- c("myDateTime", "Ereignis", "Fuellmenge", "Flasche", "Probe", "Akku")
+  columns <- c(
+    "myDateTime", "Ereignis", "Fuellmenge", "Flasche", "Probe", "Akku"
+  )
 
   actions <- kwb.utils::hsAddMissingCols(actions, columns)
 
   # in case of Probe: add bottle number
   indices.sample <- which(actions$Ereignis == "Probe")
 
-  actions$Ereignis[indices.sample] <- sprintf("Probe: Fl. %s",
-                                              kwb.utils::hsTrim(actions$Flasche[indices.sample]))
+  actions$Ereignis[indices.sample] <- sprintf(
+    "Probe: Fl. %s", kwb.utils::hsTrim(actions$Flasche[indices.sample])
+  )
 
   actions <- actions[, columns]
 
@@ -266,12 +296,23 @@ plotAllAutoSamplerActions <- function(
 
   numberOfFiles <- length(pnFiles)
 
-  for (i in 1:numberOfFiles) {
-    cat("Plotting events of sampler file", i, "/", numberOfFiles, ":", pnFiles[i], "... ")
-    actions.in.file <- all.actions[all.actions$file == pnFiles[i], ]
-    plotAutoSamplerActions(actions.in.file, fileNumber=i, group.size=group.size,
-                           evtSepTime = evtSepTime)
-    cat("ok.\n")
+  for (i in seq_len(numberOfFiles)) {
+    
+    kwb.utils::catAndRun(
+      messageText = sprintf(
+        "Plotting events of sampler file %d / %d : %s", 
+        i, numberOfFiles, pnFiles[i]
+      ), 
+      expr = {
+        actions.in.file <- all.actions[all.actions$file == pnFiles[i], ]
+        plotAutoSamplerActions(
+          actions.in.file, 
+          fileNumber = i, 
+          group.size = group.size,
+          evtSepTime = evtSepTime
+        )
+      }
+    )
   }
 }
 
@@ -284,7 +325,7 @@ plotAutoSamplerActions <- function(
   context = c(0, 0.12), evtSepTime = 30 * 60, ...
 )
 {
-  graphicalParameters <- graphics::par(no.readonly=TRUE)
+  graphicalParameters <- graphics::par(no.readonly = TRUE)
   on.exit(graphics::par(graphicalParameters))
 
   fileName <- unique(actions$file)
@@ -300,16 +341,17 @@ plotAutoSamplerActions <- function(
   top.plot.height.cm <- 4 # 3
   
   graphics::layout(
-    matrix(1:rowsPerPage, ncol = 1), 
+    matrix(seq_len(rowsPerPage), ncol = 1), 
     heights = c(graphics::lcm(top.plot.height.cm))
   )
 
   numberOfSubevents <- nrow(subevents)
 
-  for (i in 1:numberOfSubevents) {
+  for (i in seq_len(numberOfSubevents)) {
 
     # Repeat overview on each new page
     if ((i - 1) %% subevents.per.page == 0) {
+      
       graphics::par(mar=c(3,3,4,1))
 
       y1 <- 0
@@ -338,6 +380,7 @@ plotAutoSamplerActions <- function(
     actionsInEvent <- kwb.event::hsGetEvent(actions, subevents, i)
 
     if (kwb.utils::isNullOrEmpty(actionsInEvent)) {
+      
       print(actions)
       print(subevents[i, ])
       stop("no actions in subevent!")
@@ -350,15 +393,17 @@ plotAutoSamplerActions <- function(
     limits <- kwb.event::eventLimits(subevents[i, ], context=context)
 
     # Create plot area (empty plot)
-    plot.args <- list(x = x,
-                      y = rep(NA, length(x)),
-                      type = "n", # "h",
-                      xlim = c(limits$tBeg, limits$tEnd),
-                      ylim = c(-1,1),
-                      xaxt = "n",
-                      yaxt = "n",
-                      xlab = "",
-                      ylab = "")
+    plot.args <- list(
+      x = x, 
+      y = rep(NA, length(x)), 
+      xlim = c(limits$tBeg, limits$tEnd),
+      ylim = c(-1, 1), 
+      type = "n", # "h", 
+      xaxt = "n", 
+      yaxt = "n",
+      xlab = "", 
+      ylab = ""
+    )
 
     do.call(graphics::plot, plot.args)
 
@@ -374,15 +419,9 @@ plotAutoSamplerActions <- function(
 
     # labelling
     kwb.plot::addLabels(
-      x,
-      labels = labelInfo$labels,
-      alternating = TRUE,
-      col = labelInfo$colours,
-      col.line="black",
-      lty = 1,
-      lty.horiz.line = 1, # plot horizontal line
-      bandheight = 0.4,
-      group.size = 6
+      x, labels = labelInfo$labels, alternating = TRUE, col = labelInfo$colours,
+      col.line = "black", lty = 1, lty.horiz.line = 1, # plot horizontal line
+      bandheight = 0.4, group.size = 6
     )
   }
 }
@@ -401,15 +440,14 @@ logEventToLabelInfo <- function(
   indices.error <- grep("Fehler", ereignis)
 
   ereignis[indices.sample] <- sprintf(
-    "%s (%s)",
-    ereignis[indices.sample],
-    format(x[indices.sample], format="%H:%M"))
+    "%s (%s)", 
+    ereignis[indices.sample], format(x[indices.sample], format = "%H:%M")
+  )
 
   textColours <- rep("black", length(ereignis))
 
   textColours[indices.error] <- colour.error
   textColours[indices.sample] <- colour.sample
 
-  list(labels = ereignis,
-       colours = textColours)
+  list(labels = ereignis, colours = textColours)
 }
